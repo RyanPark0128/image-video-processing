535c4b59b5ea3433208a678837c3652d2e37b9f7
RyanPark0128 <vazywon@hotmail.com>
2022-05-28 22:39:39 -0700

    error fix

--- capture.py
+++ capture.py
@@ -4,12 +4,13 @@ import time
 video = cv2.VideoCapture(0)
 
 first_frame = None
+status_list = []
 
 while True:
     check, frame = video.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
+    status = 0
     if first_frame is None:
         first_frame = gray
         continue
@@ -22,14 +23,18 @@ while True:
                                  cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
+        status = 1
+
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
 
-    cv2.imshow("Gray", gray)
-    cv2.imshow("Delta", delta_frame)
-    cv2.imshow('Treshold Frame', thresh_frame)
+    status_list.append(status)
+    # cv2.imshow("Gray", gray)
+    # cv2.imshow("Delta", delta_frame)
+    # cv2.imshow('Treshold Frame', thresh_frame)
+    cv2.imshow('Color Frame', frame)
 
     key = cv2.waitKey(1)
 
